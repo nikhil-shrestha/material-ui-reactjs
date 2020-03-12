1a49d8fcfe506368ed47d84a3c9982fd964f2b36
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-12 14:34:06 +0545

    added navigation with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import theme from './ui/Theme';
 import Header from './ui/Header';
@@ -7,8 +8,36 @@ import Header from './ui/Header';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Hello!
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route
+            exact
+            path="/custom-software"
+            component={() => <div>Custom Software</div>}
+          />
+          <Route
+            exact
+            path="/mobile-apps"
+            component={() => <div>Mobile Apps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>The Revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route exact path="/contact" component={() => <div>Contacts</div>} />
+          <Route
+            exact
+            path="/estimate"
+            component={() => <div>Estimates</div>}
+          />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
