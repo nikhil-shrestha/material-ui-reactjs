0074987ca30ee2038f08a451ee99a754af1ecf04
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-12 14:12:54 +0545

    adding button to AppBar

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 
 import logo from '../../assets/logo.svg';
 
@@ -35,6 +36,13 @@ const useStyles = makeStyles(theme => ({
     ...theme.typography.tab,
     minWidth: 10,
     marginLeft: '25px'
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    marginLeft: 50,
+    marginRight: 25,
+    height: 38
   }
 }));
 
@@ -54,6 +62,13 @@ function Header(props) {
               <Tab className={classes.tab} label="About Us" />
               <Tab className={classes.tab} label="Contact Us" />
             </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
