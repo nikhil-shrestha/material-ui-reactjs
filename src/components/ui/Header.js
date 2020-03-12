168320ac48d58ff48d1d8108a5e0c35e6279c3c1
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-12 16:12:59 +0545

    added menu and setups

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -7,6 +7,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import logo from '../../assets/logo.svg';
 
@@ -55,10 +57,23 @@ const useStyles = makeStyles(theme => ({
 
 function Header(props) {
   const classes = useStyles();
+
   const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const handleChange = (e, value) => {
     setValue(value);
+  };
+
+  const handleClick = e => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpen(false);
   };
 
   useEffect(() => {
@@ -104,8 +119,11 @@ function Header(props) {
                 label="Home"
               />
               <Tab
+                aria-haspopup={anchorEl ? 'true' : undefined}
+                aria-owns={anchorEl ? 'simple-menu' : undefined}
                 className={classes.tab}
                 component={Link}
+                onMouseOver={event => handleClick(event)}
                 to="/services"
                 label="Services"
               />
@@ -135,6 +153,19 @@ function Header(props) {
             >
               Free Estimate
             </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
+            >
+              <MenuItem onClick={handleClose}>
+                Custom Software Development
+              </MenuItem>
+              <MenuItem onClick={handleClose}>Mobile App Development</MenuItem>
+              <MenuItem onClick={handleClose}>Website Development</MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
