1c8683b49a2ef37e69bf89c55cd246c06d21b981
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-13 19:58:38 +0545

    custom software development - animation row 2

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -12,8 +12,11 @@ import forwardArrow from '../assets/forwardArrow.svg';
 import lightBulb from '../assets/bulb.svg';
 import cash from '../assets/cash.svg';
 import stopWatch from '../assets/stopwatch.svg';
+import roots from '../assets/root.svg';
 import documentsAnimationData from '../animations/documentsAnimation/data';
 import scaleAnimationData from '../animations/scaleAnimation/data';
+import automationAnimationData from '../animations/automationAnimation/data';
+import uxAnimationData from '../animations/uxAnimation/data';
 
 const useStyles = makeStyles(theme => ({
   heading: {
@@ -46,6 +49,22 @@ const CustomSoftware = props => {
     loop: true,
     autoplay: false,
     animationData: scaleAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+  const automationOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: automationAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+  const uxOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: uxAnimationData,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice'
     }
@@ -116,6 +135,7 @@ const CustomSoftware = props => {
           </IconButton>
         </Grid>
       </Grid>
+
       <Grid
         item
         container
@@ -169,8 +189,9 @@ const CustomSoftware = props => {
           </Grid>
         </Grid>
       </Grid>
-      <Grid item container direction="row">
-        <Grid item container className={classes.itemContainer}>
+
+      <Grid item container direction="row" justify="space-around">
+        <Grid item container className={classes.itemContainer} md>
           <Grid item container direction="column" md>
             <Grid item>
               <Typography variant="h4">Digital Documents & Data</Typography>
@@ -195,12 +216,11 @@ const CustomSoftware = props => {
             <Lottie
               options={documentsOptions}
               isStopped={true}
-              style={{ maxHeight: 325, maxWidth: 275, minHeight: 275 }}
+              style={{ maxHeight: 275, maxWidth: 275, minHeight: 250 }}
             />
           </Grid>
         </Grid>
-
-        <Grid item container className={classes.itemContainer}>
+        <Grid item container className={classes.itemContainer} md>
           <Grid item md>
             <Lottie
               options={scaleOptions}
@@ -219,6 +239,97 @@ const CustomSoftware = props => {
                 Whether you’re a large brand, just getting started, or taking
                 off right now, our application architecture ensures pain-free
                 growth and reliability.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        container
+        direction="row"
+        style={{ marginTop: '20em', marginBottom: '20em' }}
+      >
+        <Grid item container direction="column" alignItems="center">
+          <Grid item>
+            <img
+              src={roots}
+              alt="tree with roots extending out"
+              height="450em"
+              width="450em"
+            />
+          </Grid>
+          <Grid item className={classes.itemContainer}>
+            <Typography variant="h4" align="center" gutterBottom>
+              Root-Cause Analysis
+            </Typography>
+            <Typography variant="body1" align="center" paragraph>
+              Many problems are merely symptoms of larger, underlying issues.
+            </Typography>
+            <Typography variant="body1" align="center" paragraph>
+              We can help you thoroughly examine all areas of your business to
+              develop a holistic plan for the most effective implementation of
+              technology.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item container direction="row" justify="space-around">
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4">Automation</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                Why waste time when you don’t have to?
+              </Typography>
+              <Typography variant="body1" paragraph>
+                We can help you identify processes with time or event based
+                actions which can now easily be automated.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Increasing efficiency increases profits, leaving you more time
+                to focus on your business, not busywork.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={automationOptions}
+              isStopped={true}
+              style={{ maxHeight: 290, maxWidth: 280 }}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item md>
+            <Lottie
+              options={uxOptions}
+              isStopped={true}
+              style={{ maxHeight: 310, maxWidth: 155 }}
+            />
+          </Grid>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4" align="right">
+                User Experience Design
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="right" paragraph>
+                A good design that isn’t usable isn’t a good design.
+              </Typography>
+              <Typography variant="body1" align="right" paragraph>
+                So why are so many pieces of software complicated, confusing,
+                and frustrating?
+              </Typography>
+              <Typography variant="body1" align="right" paragraph>
+                By prioritizing users and the real ways they interact with
+                technology we’re able to develop unique, personable experiences
+                that solve problems rather than create new ones.
               </Typography>
             </Grid>
           </Grid>
