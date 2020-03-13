1c925a82911131223e57d1ddba93ecd6198eede6
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-13 07:57:07 +0545

    adding social media icons and responsive factor

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -5,6 +5,9 @@ import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 
 import footerAdornment from '../../assets/Footer Adornment.svg';
+import facebook from '../../assets/facebook.svg';
+import twitter from '../../assets/twitter.svg';
+import instagram from '../../assets/instagram.svg';
 
 const useStyles = makeStyles(theme => ({
   footer: {
@@ -35,6 +38,22 @@ const useStyles = makeStyles(theme => ({
   },
   gridItem: {
     margin: '3em'
+  },
+  icon: {
+    height: '4em',
+    width: '4em',
+    [theme.breakpoints.down('xs')]: {
+      height: '2.5em',
+      width: '2.5em'
+    }
+  },
+  socialContainer: {
+    position: 'absolute',
+    marginTop: '-6em',
+    right: '1.5em',
+    [theme.breakpoints.down('xs')]: {
+      right: '0.6em'
+    }
   }
 }));
 
@@ -185,6 +204,40 @@ const Footer = props => {
         src={footerAdornment}
         className={classes.adornment}
       />
+      <Grid
+        container
+        spacing={2}
+        justify="flex-end"
+        className={classes.socialContainer}
+      >
+        <Grid
+          item
+          component={'a'}
+          href="https://www.facebook.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={facebook} alt="facebook logo" className={classes.icon} />
+        </Grid>
+        <Grid
+          item
+          component={'a'}
+          href="https://www.twitter.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={twitter} alt="twitter logo" className={classes.icon} />
+        </Grid>
+        <Grid
+          item
+          component={'a'}
+          href="https://www.instagram.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={instagram} alt="instagram logo" className={classes.icon} />
+        </Grid>
+      </Grid>
     </footer>
   );
 };
